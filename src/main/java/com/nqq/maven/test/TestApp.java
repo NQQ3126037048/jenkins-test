e6b19ecd2cd12e81f13b2bad1d3a246954db25be
NQQ3126037048 <47079212+NQQ3126037048@users.noreply.github.com>
2022-08-05 15:39:56 +0800

    Update TestApp.java

--- src/main/java/com/nqq/maven/test/TestApp.java
+++ src/main/java/com/nqq/maven/test/TestApp.java
@@ -2,7 +2,21 @@ package com.nqq.maven.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Nie QiQiang
@@ -11,11 +25,36 @@ import org.springframework.scheduling.annotation.EnableAsync;
  * @Date 2022/7/15 14:00
  */
 @EnableAsync
+@EnableSwagger2
 @SpringBootApplication
 public class TestApp {
 
     public static void main(String[] args) {
         SpringApplication.run(TestApp.class, args);
         System.out.println("项目启动成功······");
+    }
+    
+
+    @Bean
+    public Docket createRestApi() {
+        ParameterBuilder tokenPar = new ParameterBuilder();
+        List<Parameter> pars = new ArrayList<>();
+        tokenPar.name("Authorization").description("token令牌:Bearer").defaultValue("Bearer ").modelRef(new ModelRef("string")).parameterType("header").required(false).build();
+        pars.add(tokenPar.build());
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                // 为当前包路径
+                .apis(RequestHandlerSelectors.basePackage("com.nqq.maven.test.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .globalOperationParameters(pars)
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("GZ LiWan System Mgt API")
+                .version("1.0")
+                .build();
     }
 }
