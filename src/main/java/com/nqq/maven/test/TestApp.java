eecf08f700121d85986fa84db0499d711c70dd0b
NQQ3126037048 <47079212+NQQ3126037048@users.noreply.github.com>
2022-08-05 16:09:09 +0800

    Update TestApp.java
    修改swagger

--- src/main/java/com/nqq/maven/test/TestApp.java
+++ src/main/java/com/nqq/maven/test/TestApp.java
@@ -5,18 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author Nie QiQiang
@@ -34,25 +29,20 @@ public class TestApp {
         System.out.println("项目启动成功······");
     }
     
-
     @Bean
     public Docket createRestApi() {
-        ParameterBuilder tokenPar = new ParameterBuilder();
-        List<Parameter> pars = new ArrayList<>();
-        pars.add(tokenPar.build());
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 // 为当前包路径
                 .apis(RequestHandlerSelectors.basePackage("com.nqq.maven.test.controller"))
                 .paths(PathSelectors.any())
                 .build()
-                .globalOperationParameters(pars)
                 .apiInfo(apiInfo());
     }
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("jenkins-test")
+                .title("jenkins test")
                 .version("1.0")
                 .build();
     }
